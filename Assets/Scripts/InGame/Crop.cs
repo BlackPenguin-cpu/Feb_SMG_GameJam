b04e706687ffa1e03d02cd44df34c781acc905cf
Black_Penguin <dkstmdwnswns@gmail.com>
2022-02-26 04:26:01 +0900

    중간 저장

---
+++ Assets/Scripts/InGame/Crop.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crop : MonoBehaviour
+{
+    FlowerType flower;
+    private int GrowValue;
+
+    public int _GrowValue
+    {
+        get { return GrowValue; }
+        set
+        {
+            switch (flower)
+            {
+                case FlowerType.TIGER:
+                    SpriteRenderer.sprite = TigerSprites[value];
+                    break;
+                case FlowerType.KANAITION:
+                    SpriteRenderer.sprite = KanaitionSprites[value];
+                    break;
+                case FlowerType.ROSE:
+                    SpriteRenderer.sprite = RoseSprites[value];
+                    break;
+            }
+            GrowValue = value;
+        }
+    }
+
+    public bool Insect;
+    SpriteRenderer SpriteRenderer;
+ 
+    [Header("꽃 스프라이트들")]
+    [SerializeField] List<Sprite> TigerSprites;
+    [SerializeField] List<Sprite> KanaitionSprites;
+    [SerializeField] List<Sprite> RoseSprites;
+    private void Start()
+    {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+        flower = GameManager.Instance.FlowerType;
+    }
+    public void Harvest()
+    {
+        Debug.Log("수확함!");
+        _GrowValue = 0;
+    }
+}
