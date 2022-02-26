618fdc0a2706243c447f3ad6e7727a0c920ac694
Geoyeob Kim <gasbank@gmail.com>
2022-02-26 15:01:17 +0900

    Fairy.PlaySwing()으로 스윙 애니메이션 재생

--- Assets/Scripts/Fairy.cs
+++ Assets/Scripts/Fairy.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Fairy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Fairy Instance;
+    
+    [SerializeField]
+    Animator animator;
+
+    static readonly int Swing = Animator.StringToHash("Swing");
+
+    void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySwing()
     {
-        
+        animator.SetTrigger(Swing);
     }
 }
