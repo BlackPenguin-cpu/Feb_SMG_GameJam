6c755c46afb6dc3993cac558978215473f76e4b5
Geoyeob Kim <gasbank@gmail.com>
2022-02-26 22:26:37 +0900

    타이틀 스크린에 Bloom 셰이더 - 다른 씬에서 돌아오더라도 반복되지 않도록

--- Assets/Scripts/SceneMove.cs
+++ Assets/Scripts/SceneMove.cs
@@ -7,31 +7,43 @@ public class SceneMove : MonoBehaviour
 {
     public void SceneGameMain()
     {
+        if (TitlePostProcess.IsOkToInteract == false) return;
+        
         Audio.Instance.PlayButtonClick();
         SceneManager.LoadScene("GameMain");
     }
     public void SceneflowerSelection()
     {
+        if (TitlePostProcess.IsOkToInteract == false) return;
+        
         Audio.Instance.PlayButtonClick();
         SceneManager.LoadScene("flowerSelection");
     }
     public void SceneGameExplain()
     {
+        if (TitlePostProcess.IsOkToInteract == false) return;
+        
         Audio.Instance.PlayButtonClick();
         SceneManager.LoadScene("GameExplain");
     }
     public void SceneInGame()
     {
+        if (TitlePostProcess.IsOkToInteract == false) return;
+        
         Audio.Instance.PlayButtonClick();
         SceneManager.LoadScene("InGame");
     }
     public void SceneDeveloper()
     {
+        if (TitlePostProcess.IsOkToInteract == false) return;
+        
         Audio.Instance.PlayButtonClick();
         SceneManager.LoadScene("Developer");
     }
     public void SceneSound()
     {
+        if (TitlePostProcess.IsOkToInteract == false) return;
+        
         Audio.Instance.PlayButtonClick();
         SceneManager.LoadScene("Sound");
     }
--- Assets/Scripts/TitlePostProcess.cs
+++ Assets/Scripts/TitlePostProcess.cs
@@ -3,26 +3,46 @@ using UnityEngine.Rendering.PostProcessing;
 
 public class TitlePostProcess : MonoBehaviour
 {
+    static TitlePostProcess Instance;
+    
     [SerializeField]
-    PostProcessProfile profile;
-
+    PostProcessVolume volume;
+    
     Bloom bloom;
     float currentVel;
 
+    public static bool IsOkToInteract => Instance == null || Instance.bloom.intensity.value <= 10.0f;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
     void Start()
     {
-        if (profile.settings[0] is Bloom inBloom)
+        volume.profile = Instantiate(volume.profile);
+        
+        if (volume.profile.settings[0] is Bloom inBloom)
         {
             bloom = inBloom;
-            bloom.intensity.value = 100.0f;
+
+            if (Time.frameCount == 1)
+            {
+                bloom.intensity.value = 100.0f;
+            }
+            else
+            {
+                bloom.intensity.value = 0.0f;
+            }
         }
+        
     }
 
     void Update()
     {
         if (bloom != null)
         {
-            bloom.intensity.value = Mathf.SmoothDamp(bloom.intensity.value, 0, ref currentVel, 1.0f);
+            bloom.intensity.value = Mathf.SmoothDamp(bloom.intensity.value, 0, ref currentVel, 1.5f);
         }
     }
 }
